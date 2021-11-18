2d2b93ec786663293b996f06e1feb5134de35a43
unknown <gina.anifah.tif20@polban.ac.id>
2021-11-18 18:56:33 +0700

    add pertemuan 9

---
+++ Pertemuan-9/Avenged/src/AvengedCase/Flying.java
@@ -0,0 +1,7 @@
+package AvengedCase;
+
+public class Flying implements Power{
+	public void doPower() {
+		System.out.println("EAT DIRT MORTAL, BEHOLD THE POWER OF FLIGHT!");
+	}
+}
---
+++ Pertemuan-9/PaintingShape/src/PShape/Shape.java
@@ -0,0 +1,16 @@
+package PShape;
+
+public abstract class Shape {
+	protected String shapeName;
+	
+	public Shape(String Shape_name) {
+		shapeName = Shape_name;
+	}
+	//create abstract method
+	/*bertipe abstract method, maka semua yang menginstansiasikannya harus menerapkan method ini*/
+	public abstract double area();
+	
+	public String toString() {
+		return shapeName;
+	}
+}
